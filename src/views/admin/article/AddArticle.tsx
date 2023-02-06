2bc67462658e4e1ff0d331c4f332615712ffe0e1
remloyal <1182387960@qq.com>
2023-02-06 20:41:22 +0800

    tinymce中文

--- src/views/admin/article/AddArticle.tsx
+++ src/views/admin/article/AddArticle.tsx
@@ -1,7 +1,26 @@
-import React, { useRef } from 'react';
-import { Editor } from '@tinymce/tinymce-react';
+import React, { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
-function AddArticle() {
+const init = {
+  language: "zh-Hans",
+  height: "100%",
+  menubar: false,
+  auto_focus: true, //（自动获得焦点）
+  plugins: [
+    "advlist autolink lists link image charmap print preview anchor",
+    "searchreplace visualblocks code fullscreen",
+    "insertdatetime media table paste code help wordcount",
+  ],
+  toolbar:
+    "undo redo | formatselect | " +
+    "bold italic backcolor | alignleft aligncenter " +
+    "alignright alignjustify | bullist numlist outdent indent | " +
+    "removeformat | help",
+  content_style:
+    "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+};
+
+const AddArticle = () => {
   const editorRef = useRef(null);
   const log = () => {
     if (editorRef.current) {
@@ -11,29 +30,15 @@ function AddArticle() {
   return (
     <>
       <Editor
-        onInit={(evt, editor) => editorRef.current = editor}
+        onInit={(evt, editor) => (editorRef.current = editor)}
         initialValue="<p>This is the initial content of the editor.</p>"
-        tinymceScriptSrc={'/public/tinymce/js/tinymce/tinymce.min.js'}
-        apiKey='zt5s69rf909gpcjyeitoxk99uinpk8gvwpx6hwyefnm0zvls'
-        init={{
-          language: 'zh_CN',
-          height: '100%',
-          menubar: false,
-          plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount'
-          ],
-          toolbar: 'undo redo | formatselect | ' +
-            'bold italic backcolor | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | help',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-        }}
+        tinymceScriptSrc={"/public/tinymce/js/tinymce/tinymce.min.js"}
+        apiKey="zt5s69rf909gpcjyeitoxk99uinpk8gvwpx6hwyefnm0zvls"
+        init={init}
       />
       <button onClick={log}>Log editor content</button>
     </>
   );
-}
+};
 
 export default AddArticle;
